72eaeb6e37952f160b7a008deb256f3e2dd73dd6
Neel Patel <neeldpatel30@gmail.com>
2025-12-17 18:13:54 -0500

    fix one file that wasn't named correctly

--- src/pages/Calendar/components/CalendarGrid/CalendarGridEmptyDay.tsx
+++ src/pages/Calendar/components/CalendarGrid/CalendarGridEmptyDay.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { CALENDAR } from '@/app/styles/colors';
 
-const EmptyDay: React.FC = () => (
+const CalendarGridEmptyDay: React.FC = () => (
 	<div
 		className="border-r border-b p-2"
 		style={{ borderColor: CALENDAR.GRID_BORDER, color: CALENDAR.DAY_INACTIVE_TEXT, backgroundColor: CALENDAR.DAY_INACTIVE_BG }}
 	/>
 );
 
-export default EmptyDay;
+export default CalendarGridEmptyDay;
