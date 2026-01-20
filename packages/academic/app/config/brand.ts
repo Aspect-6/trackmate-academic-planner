d025230b613a15827d505e29ffcb92ada15cc047
Neel Patel <neeldpatel30@gmail.com>
2026-01-19 19:15:12 -0500

    update academic package's name

--- packages/academic/app/config/brand.ts
+++ packages/academic/app/config/brand.ts
@@ -1,4 +1,4 @@
 import { version } from '../../package.json'
 
-export const APP_FULL_NAME = 'TrackMate Academic Planner'
+export const APP_FULL_NAME = 'TrackMate Academic'
 export const CURRENT_APP_VERSION = version
