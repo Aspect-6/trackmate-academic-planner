070c22495079c2d5331f4346d651f2c59519b75b
Neel Patel <neeldpatel30@gmail.com>
2026-01-23 21:54:35 -0500

    fix inconsistent padding issue on academic Dashboard

--- packages/academic/pages/Dashboard/components/UpcomingAssignments/NoUpcomingAssignments.tsx
+++ packages/academic/pages/Dashboard/components/UpcomingAssignments/NoUpcomingAssignments.tsx
@@ -3,9 +3,9 @@ import { DASHBOARD } from '@/app/styles/colors'
 
 const NoUpcomingAssignments: React.FC = () => {
     return (
-        <div className="text-center py-6">
-            <p className="pt-4" style={{ color: DASHBOARD.TEXT_TERTIARY }}>No upcoming assignments to display.</p>
-        </div>
+        <p className="text-center py-6" style={{ color: DASHBOARD.TEXT_TERTIARY }}>
+            No upcoming assignments to display.
+        </p>
     )
 }
 
